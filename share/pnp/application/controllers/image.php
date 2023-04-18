lingej/pnp4nagios

add new url option hide_legend
this option only removes the legend and keeps the graph axis and grid.

share/pnp/application/controllers/image.php
@@ -31,6 +31,9 @@ class Image_Controller extends System_Controller  {
         if($this->input->get('graph_only') !== null)
             $this->rrdtool->config->conf['graph_only'] = 1;
 
+        if($this->input->get('hide_legend') !== null)
+            $this->rrdtool->config->conf['hide_legend'] = 1;
+
         $this->data->getTimeRange($this->start,$this->end,$this->view);
 
         if($this->tpl != ""){

ConSol/pnp

share/pnp/application/controllers/image.php
@@ -37,6 +37,9 @@ class Image_Controller extends System_Controller  {
         if($this->input->get('graph_only') !== null)
             $this->rrdtool->config->conf['graph_only'] = 1;
 
+        if($this->input->get('no_legend') !== null)
+            $this->rrdtool->config->conf['no_legend'] = 1;
+
         $this->data->getTimeRange($this->start,$this->end,$this->view);
 
         if($this->tpl != ""){